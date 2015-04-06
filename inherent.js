2a10df9a3466ab7de23d2c44098aab37e30b7907
Topher Bullock <topherbullock@gmail.com>
2015-04-06 00:47:43 -0400

    Naming and Whitespace fixes

--- inherent.js
+++ inherent.js
@@ -1,38 +1,34 @@
 var util = require("util");
 
-module.exports = function inherent ( ClassDef ) {
-
+module.exports = function inherent(ClassDef) {
   ClassDef.inherit = inherit.bind(this, ClassDef);
-
-  return ClassDef;
 };
 
-function inherit ( thisClass , superConstructor, propertyNames ) {
-  var classPrototypeClone = clone( thisClass.prototype );
+function inherit(thisClass, superConstructor, propertyNames) {
+  var classPrototypeClone = clone(thisClass.prototype);
   var constructor = Object.getOwnPropertyDescriptor(thisClass.prototype, "constructor");
 
-  if(!propertyNames) {
-    util.inherits( thisClass , superConstructor );
+  if (!propertyNames) {
+    util.inherits(thisClass, superConstructor);
   } else {
     clone(superConstructor.prototype, thisClass.prototype, propertyNames);
   }
 
   clone(classPrototypeClone, thisClass.prototype);
 
-  thisClass.prototype.super = function(){
+  thisClass.prototype.super = function() {
     this.constructor.super_.call(this);
   };
 }
 
-function clone ( classPrototype , classPrototypeClone, propertyNames ) {
-  classPrototypeClone = classPrototypeClone || {};
+function clone(classPrototype, cloneTarget, propertyNames) {
+  cloneTarget = cloneTarget || {};
   propertyNames = propertyNames || Object.getOwnPropertyNames(classPrototype);
-  propertyNames.forEach(function ( propName ) {
-
-    if( propName === "constructor"){ return; }
+  propertyNames.forEach(function(propName) {
+    if(propName === "constructor"){ return; }
     var descriptor = Object.getOwnPropertyDescriptor(classPrototype, propName);
-    Object.defineProperty( classPrototypeClone, propName, descriptor );
+    Object.defineProperty(cloneTarget, propName, descriptor);
   });
 
-  return classPrototypeClone;
+  return cloneTarget;
 }
